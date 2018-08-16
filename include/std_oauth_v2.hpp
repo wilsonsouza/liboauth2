0c8a2828620a616fa487ec98ec240fa3aaeb78c5
wilson souza <wilson.wettl@gmail.com>
2018-08-16 11:45:32 -0300

    last updated
    Changed libaulstring by STL string

--- include/std_oauth_v2.hpp
+++ include/std_oauth_v2.hpp
@@ -7,8 +7,10 @@
 *  Copyright (c) 2017 WR DevInfo
 *
 *  Created oct 10 2017
-*  Last updated
+*  Last updated 8/16/2018
 *  Version 0.1-beta
+* 
+*  Deenpencies: restbed, libjson
 */
 #pragma once
 #include <memory>
@@ -19,14 +21,14 @@ namespace std
 {
    namespace oauth_v2
    {
-      enum response : uint16_t
+      enum class response : uint16_t
       {
          CODE = 0, /*oauth2_response_code*/
          TOKEN, /*oauth2_reponse_token*/
          TOKEN_AND_CODE /*oauth2_reponse_token_and_code*/
       };
       /**/
-      enum error : uint16_t
+      enum class error : uint16_t
       {
          SUCCESS = 0,
          INVALID_REQUEST,
@@ -43,7 +45,7 @@ namespace std
       struct data_error
       {
          using pointer = shared_ptr<data_error>;
-         data_error( ) = default;
+         data_error() = default;
 
          error id;
          string description;
@@ -54,7 +56,7 @@ namespace std
       struct google_file_token_data
       {
          using pointer = shared_ptr<google_file_token_data>;
-         google_file_token_data( ) = default;
+         google_file_token_data() = default;
 
          string type;
          string project_id;
@@ -71,13 +73,13 @@ namespace std
       struct config
       {
          using pointer = shared_ptr<config>;
-         config( ) = default;
+         config() = default;
 
          string client_id;
          string client_secret;
          string redirect_uri;
          string auth_code;
-         data_error::pointer last_error = data_error::pointer( new data_error( ) );
+         data_error::pointer last_error = data_error::pointer{ new data_error{} };
       };
       /**/
       class builder
@@ -85,35 +87,36 @@ namespace std
       public:
          using pointer = shared_ptr<builder>;
       protected:
-         config::pointer m_config = config::pointer( new config( ) );
-         google_file_token_data::pointer m_google = google_file_token_data::pointer( new google_file_token_data( ) );
+         config::pointer m_config = config::pointer{ new config{} };
+         google_file_token_data::pointer m_google = google_file_token_data::pointer{ new google_file_token_data{} };
 
       public:
-         builder( ) = default;
-         builder( builder const & owner ) = delete;
-         builder( string const & client, string const & secret_key );
+         builder() = default;
+         builder(builder const & owner) = delete;
+         builder(string const & client, string const & secret_key);
+         builder & operator = (builder const &) = delete;
          /*
          *Set the redirect URI for auth code authentication.
          *This must be set before using oauth2_request_auth_code too.
          */
-         void set_redirect_uri( string const & redirect_uri );
-         void set_auth_code( string const & auth_code );
+         void set_redirect_uri(string const & redirect_uri);
+         void set_auth_code(string const & auth_code);
          /* returns URL to redirect user to*/
-         string const & request_auth_code( string const & auth_server,
-                                           string const & scope,
-                                           string const & state );
-         string const & access_auth_code( string const & auth_server,
-                                          string const & auth_code,
-                                          string const & scope );
-         string const & access_resource_owner( string const & auth_server,
-                                               string const & user_name,
-                                               string const & password );
-         string const & access_refresh_token( string const & refresh_token );
-         string const & request( string const & uri, string const & params );
+         string request_auth_code(string const & auth_server,
+                                  string const & scope,
+                                  string const & state);
+         string access_auth_code(string const & auth_server,
+                                 string const & auth_code,
+                                 string const & scope);
+         string access_resource_owner(string const & auth_server,
+                                      string const & user_name,
+                                      string const & password);
+         string access_refresh_token(string const & refresh_token);
+         string request(string const & uri, string const & params);
 
          /* set google token file data*/
-         bool set_google_file_token( string const & json_token_buffer ) noexcept;
-         google_file_token_data::pointer const & get_google_file_token_from_data( ) noexcept;
+         bool set_google_file_token(string const & json_token_buffer) noexcept;
+         google_file_token_data::pointer const & get_google_file_token_from_data() noexcept;
       };
    }
 }
--- source/std_oauth_v2.cpp
+++ source/std_oauth_v2.cpp
@@ -8,21 +8,21 @@
 *
 *  Created oct 10 2017
 *  Last updated
+*  08/16/2018 removed dependency of libaulstring.lib
 *  Version 0.1-beta
+* 
+*  Dependencies: restbed, libjson
 */
 #include <std_oauth_v2.hpp>
 #include <std_json.hpp>
-#include <std_aulstring.hpp>
 #include <restbed>
 #pragma warning(disable:4101)
 //-----------------------------------------------------------------------------------------------//
-using namespace std;
-using namespace std::oauth_v2;
 using http_request = restbed::Request;
 using builder_uri = restbed::Uri;
 using http = restbed::Http;
 //-----------------------------------------------------------------------------------------------//
-builder::builder( string const & client, string const & secret_key )
+std::oauth_v2::builder::builder(std::string const & client, std::string const & secret_key)
 {
    m_config->client_id = client;
    m_config->client_secret = secret_key;
@@ -33,136 +33,136 @@ builder::builder( string const & client, string const & secret_key )
 *Set the redirect URI for auth code authentication.
 *This must be set before using oauth2_request_auth_code too.
 */
-void builder::set_redirect_uri( string const & redirect_uri )
+void std::oauth_v2::builder::set_redirect_uri(std::string const & redirect_uri)
 {
    m_config->redirect_uri = redirect_uri;
 }
 //-----------------------------------------------------------------------------------------------//
-void builder::set_auth_code( string const & auth_code )
+void std::oauth_v2::builder::set_auth_code(std::string const & auth_code)
 {
    m_config->auth_code = auth_code;
 }
 //-----------------------------------------------------------------------------------------------//
 /* returns URL to redirect user to*/
-string const & builder::request_auth_code( string const & auth_server,
-                                           string const & scope,
-                                           string const & state )
+std::string std::oauth_v2::builder::request_auth_code(std::string const & auth_server,
+                                                      std::string const & scope,
+                                                      std::string const & state)
 {
-   auto data = ansi_string( auth_server );
+   auto data = string{ auth_server };
    /**/
-   data.append( "?response_type=code" );
-   data.append( "&client_id=" ).append( m_config->client_id );
-   data.append( "&redirect_uri=" ).append( m_config->redirect_uri );
+   data.append("?response_type=code");
+   data.append("&client_id=").append(m_config->client_id);
+   data.append("&redirect_uri=").append(m_config->redirect_uri);
    /**/
-   if( !scope.empty( ) )
+   if (!scope.empty())
    {
-      data.append( "&scope=" ).append( scope );
+      data.append("&scope=").append(scope);
    }
    /**/
-   if( !state.empty( ) )
+   if (!state.empty())
    {
-      data.append( "&state=" ).append( state );
+      data.append("&state=").append(state);
    }
    /**/
-   return data.reference( );
+   return data;
 }
 //-----------------------------------------------------------------------------------------------//
-string const & builder::access_auth_code( string const & auth_server,
-                                          string const & auth_code,
-                                          string const & scope )
+std::string std::oauth_v2::builder::access_auth_code(std::string const & auth_server,
+                                                     std::string const & auth_code,
+                                                     std::string const & scope)
 {
-   auto data = ansi_string( auth_server );
+   auto data = string{ auth_server };
    /**/
-   data.append( "grant_type=authorization_code" );
-   data.append( "&client_id=" ).append( m_config->client_id );
-   data.append( "&client_secret=" ).append( m_config->client_secret );
-   data.append( "&code=" ).append( auth_code );
-   data.append( "&redirect_uri=" ).append( m_config->redirect_uri );
+   data.append("grant_type=authorization_code");
+   data.append("&client_id=").append(m_config->client_id);
+   data.append("&client_secret=").append(m_config->client_secret);
+   data.append("&code=").append(auth_code);
+   data.append("&redirect_uri=").append(m_config->redirect_uri);
    /* make call */
-   auto req = make_shared<http_request>( builder_uri( data ) );
-   auto res = http::sync( req );
-   auto body = res->get_body( );
+   auto req = make_shared<http_request>(builder_uri(data));
+   auto res = http::sync(req);
+   auto body = res->get_body();
    /**/
-   data.assign( string( body.begin( ), body.end( ) ) );
-   return data.reference( );
+   data.assign(string(body.begin(), body.end()));
+   return data;
 }
 //-----------------------------------------------------------------------------------------------//
-string const & builder::access_resource_owner( string const & auth_server,
-                                               string const & user_name,
-                                               string const & password )
+std::string std::oauth_v2::builder::access_resource_owner(std::string const & auth_server,
+                                                          std::string const & user_name,
+                                                          std::string const & password)
 {
-   auto data = ansi_string( auth_server );
+   auto data = string{ auth_server };
    /**/
-   data.append( "grant_type=password" );
-   data.append( "&client_id" ).append( m_config->client_id );
-   data.append( "&username=" ).append( user_name );
-   data.append( "&password=" ).append( password );
+   data.append("grant_type=password");
+   data.append("&client_id").append(m_config->client_id);
+   data.append("&username=").append(user_name);
+   data.append("&password=").append(password);
    /* make call uri */
-   auto req = make_shared<http_request>( builder_uri( data ) );
-   auto res = http::sync( req );
-   auto body = res->get_body( );
+   auto req = make_shared<http_request>(builder_uri(data));
+   auto res = http::sync(req);
+   auto body = res->get_body();
    /**/
-   data.assign( string( body.begin( ), body.end( ) ) );
-   return data.reference( );
+   data.assign(string(body.begin(), body.end()));
+   return data;
 }
 //-----------------------------------------------------------------------------------------------//
-string const & builder::access_refresh_token( string const & refresh_token )
+std::string std::oauth_v2::builder::access_refresh_token(std::string const & refresh_token)
 {
-   return ansi_string( ).reference( );
+   return string{};
 }
 //-----------------------------------------------------------------------------------------------//
-string const & builder::request( string const & uri, string const & params )
+std::string std::oauth_v2::builder::request(std::string const & uri, std::string const & params)
 {
-   auto data = ansi_string( uri );
+   auto data = string{ uri };
    /**/
-   if( uri.empty( ) )
+   if (data.empty())
    {
-      return data.reference( );
+      return data;
    }
    /**/
-   data.append( params );
-   data.append( "&access_token=" ).append( m_config->auth_code );
+   data.append(params);
+   data.append("&access_token=").append(m_config->auth_code);
    /* make call */
-   auto req = make_shared<http_request>( builder_uri( data ) );
-   auto res = http::sync( req );
-   auto body = res->get_body( );
+   auto req = make_shared<http_request>(builder_uri(data));
+   auto res = http::sync(req);
+   auto body = res->get_body();
    /**/
-   data.assign( string( body.begin( ), body.end( ) ) );
-   return data.reference( );
+   data.assign(string(body.begin(), body.end()));
+   return data;
 }
 //-----------------------------------------------------------------------------------------------//
 /* set google token file data*/
-bool builder::set_google_file_token( string const & json_token_buffer ) noexcept
+bool std::oauth_v2::builder::set_google_file_token(std::string const & json_token_buffer) noexcept
 {
    try
    {
-      auto body = json::value::parse( json_token_buffer );
+      auto body = json::value::parse(json_token_buffer);
       /**/
-      if( body.empty( ) )
+      if (body.empty())
       {
-         throw std::exception( "client file key is empty!!!" );
+         throw std::exception("client file key is empty!!!");
       }
       /**/
-      m_google->auth_provider_x509_cert_url = body[ "auth_provider_x509_cert_url" ].get<string>( );
-      m_google->auth_uri = body[ "auth_uri" ].get<string>( );
-      m_google->client_email = body[ "client_email" ].get<string>( );
-      m_google->client_id = body[ "client_id" ].get<string>( );
-      m_google->client_x509_cert_url = body[ "client_x509_cert_url" ].get<string>( );
-      m_google->private_key = body[ "private_key" ].get<string>( );
-      m_google->private_key_id = body[ "private_key_id" ].get<string>( );
-      m_google->project_id = body[ "project_id" ].get<string>( );
-      m_google->token_uri = body[ "token_uri" ].get<string>( );
-      m_google->type = body[ "type" ].get<string>( );
+      m_google->auth_provider_x509_cert_url = body["auth_provider_x509_cert_url"].get<string>();
+      m_google->auth_uri = body["auth_uri"].get<string>();
+      m_google->client_email = body["client_email"].get<string>();
+      m_google->client_id = body["client_id"].get<string>();
+      m_google->client_x509_cert_url = body["client_x509_cert_url"].get<string>();
+      m_google->private_key = body["private_key"].get<string>();
+      m_google->private_key_id = body["private_key_id"].get<string>();
+      m_google->project_id = body["project_id"].get<string>();
+      m_google->token_uri = body["token_uri"].get<string>();
+      m_google->type = body["type"].get<string>();
       /**/
       return true;
    }
-   catch( std::exception const & e )
+   catch (std::exception const & e)
    {
    }
    return false;
 }
 //-----------------------------------------------------------------------------------------------//
-google_file_token_data::pointer const & builder::get_google_file_token_from_data( ) noexcept
+std::oauth_v2::google_file_token_data::pointer const & std::oauth_v2::builder::get_google_file_token_from_data() noexcept
 {
    return m_google;
 }
